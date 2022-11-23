ad17aaad7f8161dffd0109dcf105660469eb3a83
HannesKirsten <hannes.kirsten@labs.epiuse.com>
2022-11-23 22:05:21 +0200

    Added flag infected survivor functionality

--- app/api/flag_infected.py
+++ app/api/flag_infected.py
@@ -1,10 +1,24 @@
+import sqlite3
+
 from flask_restful import Resource
 from . import api
 
 
 class FlagInfectedSurvivor(Resource):
-    def get(self, name: str):
-        return {"message": f"Hello {name}"}
+    def put(self, name: str):
+
+        db = sqlite3.connect('survivor.db')
+        statement = ''' UPDATE survivors SET infected = 1 WHERE name = ?'''
+
+        cursor = db.cursor()
+
+        try:
+            cursor.execute(statement, (name,))
+            db.commit()
+        except sqlite3.IntegrityError as error:
+            return {'status': 'error', 'message': error.args}
+
+        return {'status': 'success', 'message': f'{name} is now infected'}
 
 
-api.add_resource(FlagInfectedSurvivor, '/api/flag-infected-survivor/<string:name>')
+api.add_resource(FlagInfectedSurvivor, '/flag-infected-survivor/<string:name>')
