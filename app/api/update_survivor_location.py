5e44137a540aa872381c6c99af486e865fc8adf7
HannesKirsten <hannes.kirsten@labs.epiuse.com>
2022-11-23 22:34:11 +0200

    Update location documentation added

--- app/api/update_survivor_location.py
+++ app/api/update_survivor_location.py
@@ -1,12 +1,18 @@
 import sqlite3
 
 from flask_restful import Resource, reqparse
+from flask_restful_swagger import swagger
+
 from . import api
 
 reqparser = reqparse.RequestParser()
 reqparser.add_argument('last_location', type=str)
 
 class UpdateSurvivorLocation(Resource):
+    """Update the location of a specified survivor"""
+    @swagger.operation(
+        notes='Specify a new location in the format \'longitude, latitude\''
+    )
     def put(self, name: str):
         arguments = reqparser.parse_args()
         print(arguments)
